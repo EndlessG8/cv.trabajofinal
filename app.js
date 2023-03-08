973e960b5530bf9aa33d3b45ee73617c9b34f912
EndlessG8 <126737602+EndlessG8@users.noreply.github.com>
2023-03-08 12:40:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ function mostrarOcultarMenu(){
     }    
 }
 
-let links = document.querySelectorAll("nav");
+let links = document.querySelectorAll("nav a");
 for(var x = 0; x <links.length; x++){
-    links[x].onlick = function(){
+    links[x].onclick = function(){
         menu.style.display = "none";
         menu_visible = false;
     }
